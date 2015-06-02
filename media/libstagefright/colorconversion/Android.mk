b63d04bfb094d02586ad05434742378ab5da7e8f
Huisung Kang <hs1218.kang@samsung.com>
2015-06-02 21:15:15 +0200

    libstagefright: YV12 alignment modification is enabled for softwarerenderer YV12 chroma stide should be aligned to 16 according to the android framework. But, GSC & Mali couldn't accept a seperate stride for U & V components. They just use Y_stride/2 as a UV Stride.
    Change-Id: I6b6bc992ecae650399c9428176a833c2b8cc0c8b
    Signed-off-by: Huisung Kang <hs1218.kang@samsung.com>

--- media/libstagefright/colorconversion/Android.mk
+++ media/libstagefright/colorconversion/Android.mk
@@ -9,6 +9,10 @@ LOCAL_C_INCLUDES := \
         $(TOP)/frameworks/native/include/media/openmax \
         $(TOP)/hardware/msm7k
 
+ifeq ($(BOARD_USE_MALI_ALIGNMENT),true)
+  LOCAL_CFLAGS += -DMALI_ALIGNMENT
+endif
+
 LOCAL_MODULE:= libstagefright_color_conversion
 
 include $(BUILD_STATIC_LIBRARY)
